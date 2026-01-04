b00266036fa675c5dcfedb711bfbf9c64af6bb71
Arvind-Vishwa <av0216380@gmail.com>
2026-01-04 07:33:10 +0530

    add Insertion sort

---
+++ sorting/insertion.sort.cpp
@@ -0,0 +1,31 @@
+// insertion sort
+
+#include<iostream>
+using namespace std;
+
+
+void insertsort(int arr[],int n){
+
+    for(int i=1;i<n;i++){
+        int temp=arr[i];
+        int j=i-1;
+        for(;j>=0;j--){
+            if(arr[j] > temp){
+                // shift
+                arr[j+1]=arr[j];
+            }else
+            break;
+        }
+        arr[j+1]=temp;
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int arr[]={10,1,7,4,8,2,11};
+    int n=7;
+    insertsort(arr,n);
+}
