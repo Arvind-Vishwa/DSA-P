e3f79830ab7809a2f245cb5f0cdc0d66730822f8
Arvind-Vishwa <av0216380@gmail.com>
2026-01-30 18:37:46 +0530

    Optimized HCF

--- Revision/Day1/findHCF.cpp
+++ Revision/Day1/findHCF.cpp
@@ -1,39 +1,27 @@
-// Find HCF/GCD
 #include<bits/stdc++.h>
 using namespace std;
 
-void findHCF(int a,int b){
-    // small no
-    // finding the factor of all a and b
+void findHCF(int a, int b) {
+    vector<int> arr1, arr2;
 
-    vector<int>arr1;
-    vector<int>arr2;
+    // Corrected loop ranges
+    for(int i = 1; i <= a; i++) if(a % i == 0) arr1.push_back(i);
+    for(int i = 1; i <= b; i++) if(b % i == 0) arr2.push_back(i);
 
-    for(int i=1;i<=a;i++){
-        if(a%i == 0){
-            arr1.push_back(i);
+    int max_ans = 1;
+    // Nested loop to find common elements
+    for(int factorA : arr1) {
+        for(int factorB : arr2) {
+            if(factorA == factorB) {
+                max_ans = max(max_ans, factorA);
+            }
         }
     }
-
-    for(int i=1;i<=a;i++){
-        if(b%i == 0){
-            arr2.push_back(i);
-        }
-    }
-    int ans=1;
-    int max_ans=INT_MIN;
-    
-    for(auto k:arr1){
-        if(arr1[k] == arr2[k]){
-            ans = arr1[k];
-        }
-        max_ans=max(max_ans,ans);
-    }
-    cout<<max_ans<<" ";
+    cout << max_ans << " ";
 }
 
 int main(){
-    int a=8;
-    int b=4;
+    int a=4;
+    int b=8;
     findHCF(a,b);
 }
