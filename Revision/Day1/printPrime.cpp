18f7db818140ae69652840457d1f349302ed7c5f
Arvind-Vishwa <av0216380@gmail.com>
2026-02-02 19:12:44 +0530

    upated Prime

--- Revision/Day1/printPrime.cpp
+++ Revision/Day1/printPrime.cpp
@@ -1,11 +1,10 @@
-// Print prime No from 1 to N
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-void printPrime(int n){
-    // brute force
-    int arr[n]={0};
-    
+void findPrime(int n){
+    // brute
+    // jo no khud se ya 1 divide hota h prime
+    vector<int>ans;
     for(int i=1;i<=n;i++){
         int cnt=0;
         for(int j=1;j<=n;j++){
@@ -14,16 +13,17 @@ void printPrime(int n){
             }
         }
         if(cnt == 2){
-            arr[i-1]=i;
+            ans.push_back(i);
         }
+
     }
 
-    for(auto k:arr){
+    for(auto k:ans){
         cout<<k<<" ";
     }
 }
 
 int main(){
-    int n=10;
-    printPrime(n);
+    int n=20;
+    findPrime(n);
 }
