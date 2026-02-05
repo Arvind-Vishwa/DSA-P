2492d661d94ced273614eb06794b90f38f7d7757
Arvind-Vishwa <av0216380@gmail.com>
2026-02-05 22:33:13 +0530

    update sqrt

--- Revision/Day4/findSqrt.cpp
+++ Revision/Day4/findSqrt.cpp
@@ -18,24 +18,23 @@ void findSqrt(int n){
 
 int binarySqr(int n){
     int s=0;
-    int e=n/2;
-
+    int e=n;
+    int ans=0;
     while(s <= e){
         int mid=(s+e)/2;
-        if(mid*mid == n){
-            return mid;
-        }
-        else if(mid*mid > n){
-            e=mid-1;
-        }else{
+        if(mid*mid <= n){
+            ans=mid;
             s=mid+1;
         }
+        else{
+            e=mid-1;
+        }
     }
-    return -1;
+    return ans;
 }
 
 int main(){
-    int n=81;
+    int n=72;
     // findSqrt(n);
     int ans=binarySqr(n);
     cout<<ans<<endl;
