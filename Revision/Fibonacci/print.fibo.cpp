a69615fd9020027eedac6ceb51de0f5e3bcf7e6b
Arvind-Vishwa <av0216380@gmail.com>
2026-02-06 22:07:15 +0530

    print fibo

---
+++ Revision/Fibonacci/print.fibo.cpp
@@ -0,0 +1,22 @@
+// print fibonacci upto n terms
+#include<iostream>
+using namespace std;
+
+void printFibo(int n){
+    int arr[n]={0};
+
+    arr[0]=0;
+    arr[1]=1;
+    int sum=0;
+    for(int i=2;i<=n;i++){
+        sum=arr[i-1]+arr[i-2];
+        arr[i]=sum;
+        cout<<sum<<" ";
+    }
+    
+}
+
+int main(){
+    int n=10;
+    printFibo(n);
+}
