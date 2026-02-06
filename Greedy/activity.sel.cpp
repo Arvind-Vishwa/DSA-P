9d77f8b1aa893e8fe4accfa66d68d70fef2fa41b
Arvind-Vishwa <av0216380@gmail.com>
2026-02-06 07:35:32 +0530

    Activity Selection

---
+++ Greedy/activity.sel.cpp
@@ -0,0 +1,36 @@
+// Activity selection
+#include<bits/stdc++.h>
+using namespace std;
+
+bool comp(const auto&a,const auto&b){
+    return b.second > a.second;
+}
+
+void findActivity(vector<int>&start,vector<int>&finish){
+    vector<pair<int,int>>temp;
+
+    for(int i=0;i<start.size();i++){
+        temp.push_back({start[i],finish[i]});
+    }
+
+    sort(temp.begin(),temp.end(),[](auto&a,auto&b){
+        return a.second < b.second;
+    });
+
+    int curr_time=temp[0].second;
+    int cnt=1;
+    for(int i=1;i<temp.size();i++){
+        if(temp[i].first > curr_time){
+            cnt++;
+        }
+        curr_time=temp[i-1].second;
+    }
+
+    cout<<"ans :"<<cnt;
+}
+
+int main(){
+    vector<int>start={1,3,0,5,8,5};
+    vector<int>finish={2,4,6,7,9,9};
+    findActivity(start,finish);
+}
